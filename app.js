265f5da5da87e4d3fd85bcefc1e04bf300c78224
chiedo07 <chiedoluiz@gmail.com>
2023-04-10 13:53:00 +0100

    final

--- app.js
+++ app.js
@@ -18,3 +18,27 @@ for(i = 0; i < hide.length; i++) {
     }
 }
 // click to add list items
+function Addlist(){
+    var li = document.createElement("LI");
+    var myvalue = document.getElementById("task").value;
+    var text = document.createTextNode(myvalue);
+    li.appendChild(text);
+    if(myvalue === ""){
+        alert("You Must Write Something")
+    } else{document.getElementById("myli").appendChild(li)
+}
+document.getElementById("task").value ="";
+
+var span = document.createElement("span");
+var txt = document.createTextNode("\u00D7");
+span.className = "close";
+span.appendChild(txt);
+li.appendChild(span);
+
+for(i = 0; i < hide.length; i++){
+hide[i].onclick = function(){
+    var rid = this.parentElement;
+    rid.style.display= "none";
+}
+}
+}
